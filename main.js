a3f2391a24775bc569b01f47573ca63815f01246
Vuyo Kumalo <vuyokumalo2.o@gmail.com>
2025-07-01 15:24:32 +0300

    My portfolio is ready to go online with a plenty room for improvement

---
+++ main.js
@@ -0,0 +1,13 @@
+//alert("Wola, Ntanga!");//
+//document.querySelector('lightbtn').onclick = function () {document.querySelector('body').classList.toggle('light');}
+//document.querySelector('darkbtn').onclick = function () {document.querySelector('body').classList.toggle('dark');}
+const lightbtn = document.getElementById("lightbtn");
+const darkbtn = document.getElementById("darkbtn");
+lightbtn.addEventListener("click", () => {
+  document.body.classList.add("light");
+  document.body.classList.remove("dark");
+});
+darkbtn.addEventListener("click", () => {
+  document.body.classList.add("dark");
+  document.body.classList.remove("light");
+});
